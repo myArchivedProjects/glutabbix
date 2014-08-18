b3e429ca33044229c379a252d21c3ddca35ffcab
Azul <mail@azulinho.com>
2014-08-18 16:00:18 +0100

    version 0.0.3

--- glutabbix/setup.py
+++ glutabbix/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='glutabbix',
-      version='0.0.2',
+      version='0.0.3',
       url='https://github.com/Azulinho/glutabbix',
       py_modules=['glutabbix'],
       )
