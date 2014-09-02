fd442fe61b54f5c86b3e8cd7085dba49b612f0bb
Azul <mail@azulinho.com>
2014-09-02 21:39:01 +0100

    added requests dependency

--- glutabbix/setup.py
+++ glutabbix/setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 setup(name='glutabbix',
-      version='0.0.9',
+      version='0.0.10',
       url='https://github.com/Azulinho/glutabbix',
       py_modules=['glutabbix'],
+      install_requires=['requests'],
       )
